ed1c9b4630307d1a74b7eb321d155582f4a6cd50
B1_13020210040_Nurmila Sari R. Usia <ichaichaiic.63@gmail.com>
2023-03-12 10:33:44 +0700

    first

---
+++ Tugas1_SourceCode_Praktek dan Tugas1_SourceCode_Kasus/asign.java
@@ -0,0 +1,23 @@
+/*NIM 		: 13020210023
+NAMA		: ANNISA PRATAMA PUTRI 
+HARI/TANGGAL 	: SABTU,11 MARET 2023*/
+*/
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javautilscanne;
+
+/**
+ *
+ * @author Asus
+ */
+public class asign {
+    public static void main(String[] args) {
+        int i;
+        
+        System.out.print ("hello\n"); i=5;
+        System.out.println("Ini nilai i : "+ i);
+    }
+    
+}
+
